2ed40bff18c39d5eff4ff908740fe8e8b5552f6f
ProgSomel <somelahmed88@gmail.com>
2024-05-09 18:10:30 +0600

    Trip section done

--- dpt-frontend-task/src/App.jsx
+++ dpt-frontend-task/src/App.jsx
@@ -1,12 +1,29 @@
 
-import './App.css'
+import { useEffect } from 'react'
 
 function App() {
 
+  useEffect(()=> {
+
+  }, [])
+
   return (
-    <>
+    <div>
+      <h1 className='max-w-5xl mx-auto mt-1 font-bold text-xl'>Master Price</h1>
       
-    </>
+      <hr />
+      {/*! Trip Type Section  */}
+      <div className='flex justify-center mt-3 mb-4 max-w-5xl mx-auto border-b py-5 border-blue-400 font-bold text-xl'>
+        <button className='border px-2 font-bold border-black text-xs'>Round Trip</button>
+        <button className='border px-2 font-bold border-black bg-blue-900 text-white text-xs '>One Way</button>
+        <button className='border px-2 font-bold border-black text-xs '>Multi City</button>
+      </div>
+
+      <div>
+
+      </div>
+      
+    </div>
   )
 }
 
